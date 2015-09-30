acf4fc2c2425c26fbb65c41933165c93f4e93105
mcarter <mrmichaelcarter822@gmail.com>
2015-09-30 13:51:44 -0400

    DONE

---
+++ Area/src/Area.java
@@ -0,0 +1,22 @@
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am going to calculate the area of a circle")
+double circleareainMainMethod = circleArea(10)
+System.out.println("Area of the circle is "+ circleareainMainMethod );
+	
+
+			double areaC = circleArea(35);
+			System.out.println(areaC);
+		}
+		public static double circleArea(int radius){
+			
+			
+			double area = 3.14*radius*radius;
+			return area;
+		}
+
+	}
+

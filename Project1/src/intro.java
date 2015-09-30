11f2d1ea9b375b0d7a105d9f15d8030b3ca0f7a5
mcarter <mrmichaelcarter822@gmail.com>
2015-09-30 13:48:16 -0400

    Im done

---
+++ Project1/src/intro.java
@@ -0,0 +1,11 @@
+
+public class intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Michael Carter");
+  System.out.println("My Favorite book is Divergent ");
+  System.out.println(" My favorite color is purple ");
+	}
+
+}
